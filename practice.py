432e1d2538b032aa0a6351f0490c1699b9035c08
dinahslsbla <dinahslsbla@gmail.com>
2023-11-08 18:31:02 +0700

    first commit

---
+++ practice.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = '7600e864-f049-4486-a566-0840a0234148'
+word = 'potato'
+url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}'
+
+res = requests.get(url)
+
+definitions = res.json()
+
+for definition in definitions:
+    print(definition)
